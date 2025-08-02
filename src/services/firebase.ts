164eea3aabb228e5c26c4c6031e11f272fdbefac
TerraTAC <admin@terratac.co.uk>
2025-08-02 18:47:45 +0100

    Initial frontend setup

---
+++ src/services/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqYr9nGEPXWMvSMgf6LKVlIt5Y4gMYkVM",
+  authDomain: "meeting-muse-8saaa.firebaseapp.com",
+  databaseURL: "https://meeting-muse-8saaa-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "meeting-muse-8saaa",
+  storageBucket: "meeting-muse-8saaa.firebasestorage.app",
+  messagingSenderId: "945943283107",
+  appId: "1:945943283107:web:308f715fd717ea67f44616"
+};
